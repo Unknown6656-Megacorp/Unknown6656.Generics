c829678a28b5cfea00ba3ec39c18a5e6fe4101c5
unknown6656 <unknown84@gmx.de>
2022-04-09 13:23:21 +0200

    added Box<T>

---
+++ Box.cs
@@ -0,0 +1,44 @@
+﻿using System.Runtime.CompilerServices;
+using System;
+
+
+namespace Unknown6656.Generics;
+
+
+/// <summary>
+/// A generic data type for boxing and unboxing <see langword="struct"/>s.
+/// </summary>
+/// <typeparam name="T">The <see langword="struct"/>'s generic type.</typeparam>
+public sealed class Box<T>
+    : IEquatable<Box<T>>
+    where T : struct
+{
+    public T Data;
+
+    public ref T Reference => ref Data;
+
+
+    public Box()
+        : this(default)
+    {
+    }
+
+    public Box(T value) => Data = value;
+
+    public bool Equals(Box<T>? other) => Data.Equals(other?.Data);
+
+    public override bool Equals(object? obj) => obj is Box<T> box && Equals(box);
+
+    public override string? ToString() => Data.ToString();
+
+    public override int GetHashCode() => Data.GetHashCode();
+
+
+    public static bool operator ==(Box<T>? box1, Box<T>? box2) => box1?.Equals(box2) ?? box2 is null;
+
+    public static bool operator !=(Box<T>? box1, Box<T>? box2) => !(box1 == box2);
+
+    public static implicit operator T(Box<T> box) => box.Data;
+
+    public static implicit operator Box<T>(T data) => new() { Data = data };
+}
